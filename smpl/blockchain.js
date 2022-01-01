7e7874f14fb68ebea9e859284c28bf6f13d12c36
eumcloud <livelch012@naver.com>
2022-01-01 11:59:47 +0000

    target init

--- smpl/blockchain.js
+++ smpl/blockchain.js
@@ -1,12 +1,12 @@
 class Blockchain{
-	construcor(){
+	constructor(){
 	this.chain = [];
 	this.newTransactions = [];
 	}
 }
 
-Blockchain.prototype.createNewBlock = function (
-	nonce, previousBlockHash, hash){
+Blockchain.prototype.createNewBlock 
+	= function (nonce, previousBlockHash, hash){
 		const newBlock = {
 			index: this.chain.length +1,
 			timestamp: Date.new(),

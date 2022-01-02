2674a9cb4f0cd6fe2300677793ed5a3b9ac64b0a
eumcloud <livelch012@naver.com>
2022-01-02 13:34:27 +0000

    dummy createblock print

--- temp/dev/test.js
+++ temp/dev/test.js
@@ -3,6 +3,6 @@ const Blockchain = require("./blockchain");
 //인스턴스
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock();
+bitcoin.createNewBlock(2389, "OIUOEREDHKHKD", "78S97D4X6DSF");
 
 console.log(bitcoin);
